5fe16e67c4519d3e3a84cc1b6df67ff2939bf362
isa's sanctuary <khrnsa143@gmail.com>
2025-09-08 08:21:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Tugas OOPTIGA</title>
+  </head>
+  <body>
+    <h1>hi, selamat datang di tugas praktikum OOP 1</h1>
+    <?php echo '<p>Pilih salah satu</p>'; ?> 
+    <a href="ppbo3_login.php">LOGIN TERLEBIH DAHULU</a>
+    <a href="ppbo3_persegipanjang.php">Latihan 1</a>
+    <a href="ppbo3_produk.php">Latihan 2</a>
+    <a href="ppbo3_persegipanjang.php">Latihan1</a>
+  </body>
+</html>
+?>
