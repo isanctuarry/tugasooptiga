1b214f58efa562d53b68cd47c6dc547a551b1927
isa's sanctuary <khrnsa143@gmail.com>
2025-09-08 08:27:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
   <head>
-    <title>Tugas OOPTIGA</title>
+    <title>Tugas OOP TIGA</title>
   </head>
   <body>
-    <h1>hi, selamat datang di tugas praktikum OOP 1</h1>
+    <h1>HI, selamat datang di tugas OOP 3</h1>
     <?php echo '<p>Pilih salah satu</p>'; ?> 
     <a href="ppbo3_login.php">LOGIN TERLEBIH DAHULU</a>
     <a href="ppbo3_persegipanjang.php">Latihan 1</a>
     <a href="ppbo3_produk.php">Latihan 2</a>
-    <a href="ppbo3_persegipanjang.php">Latihan 3</a>
+    <a href="ppbo3_perpustakaan.php">Latihan 3</a>
   </body>
 </html>
 ?>
