5dcfc3a8b44167e029d937a7f662115df3c8b15b
isa's sanctuary <khrnsa143@gmail.com>
2025-09-19 20:53:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,67 @@
 <?php
-class Mahasiswa {
-    public $nama;
-    public $nim;
-    public $prodi;
-    protected $ipk;     
-    private $password;  
-
-    public function setNilaiIPK($ipk) {
-        $this->ipk = $ipk;
-    }
-
-    public function setPassword($password) {
-        $this->password = $password;
-    }
-    public function showNilaiIPK() {
-        return $this->getNilaiIPK(); // memanggil method protected
-    }
-
-    public function showPassword() {
-        return $this->getPassword(); // memanggil method private
-    }
-
-    protected function getNilaiIPK() {
-        return "Nilai IPK: $this->ipk";
-    }
-
-    private function getPassword() {
-        return "Password: $this->password";
-    }
-}
-
-$mahasiswa = new Mahasiswa();
-$mahasiswa->nama = "Claude Clawmark";
-$mahasiswa->nim = "012223";
-$mahasiswa->prodi = "Teknik Informatika";
-
-$mahasiswa->setNilaiIPK(3.9);
-$mahasiswa->setPassword("yummyno");
-
-
-echo "Nama: " . $mahasiswa->nama . "<br>";
-echo "NIM: " . $mahasiswa->nim . "<br>";
-echo "Prodi: " . $mahasiswa->prodi . "<br>";
-echo $mahasiswa->showNilaiIPK() . "<br>";
-echo $mahasiswa->showPassword() . "<br>";
-
+// Daftar file latihan  
+$latihan = [
+    "ppbo3_produk.php" => "Latihan 1: Produk",
+    "ppbo3_login" => "Latihan 2: Login",
+    "ppbo3_perpustakaan.php" => "Latihan 3: Perpustakaan",
+    "ppbo3_persegipanjang.php"=> "Latihan 6: Hitung Luas Persegi Panjang",
+    "tugasoop5.php"=> "Latihan Praktikum 5",
+    "tugasoop5pt2.php"=> "Latihan Praktikum 5.2",
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Latihan OOP</title>
+     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Poppins:wght@300;500&display=swap" rel="stylesheet">
+    <style>
+        body { 
+            font-family: Poppins; 
+            margin: 0; 
+            background: #f9f9f9; 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 { 
+            color: #fdb2b2ff; 
+            font-family: Poppins;
+            text-align: center;
+        }
+
+        ul {
+            list-style: none;
+             padding: 0; 
+             margin: 0;
+            }
+
+        li { 
+            margin: 15px 0;
+            }
+            
+        a { 
+            text-decoration: none; 
+            color: white; 
+            background: #fcbae0ff; 
+            padding: 10px 15px; 
+            border-radius: 5px; 
+            display: inline-block;  
+            align-items: center;
+        }
+        a:hover { background: #f174e8ff; }
+        
+    </style>
+</head>
+<body>
+    <h1>Daftar Latihan OOP</h1>
+    <ul>
+        <?php foreach ($latihan as $file => $judul): ?>
+            <li><a href="<?= $file ?>"><?= $judul ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
